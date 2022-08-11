0ee2d2c83bd27b22559e700def6bfd8f58bdeb55
Laisses <71999249+Laisses@users.noreply.github.com>
2022-08-11 09:55:24 -0300

    habilita botao com condicoes

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const isChecked = () => {
     const sobremesas = document.querySelector("input[name=sobremesa]:checked");
 
     if (pratos !== null && bebidas !== null && sobremesas !== null) {
-        console.log("deu certo")
+        enableBtn();
     }
 };
+
+const enableBtn = () => {
+    const btn = document.querySelector("button");
+        
+    btn.textContent = "Fechar pedido";
+    btn.style.backgroundColor = "#32B72F";
+    btn.removeAttribute("disabled");
+}
 
 document.querySelectorAll("form").forEach(form => {
     form.onclick = isChecked;
